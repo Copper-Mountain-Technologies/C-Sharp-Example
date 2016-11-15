3a17c63dbf75148cdb3c0c400acbceefec35ed41
Marc McClanahan <marc.m@coppermountaintech.com>
2016-11-15 11:13:06 -0500

    Add project files.

---
+++ CSharpExample/FormMain.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace CSharpExample
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(18, 18);
+            this.buttonStart.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(112, 35);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // textBox
+            // 
+            this.textBox.BackColor = System.Drawing.Color.White;
+            this.textBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox.Location = new System.Drawing.Point(20, 63);
+            this.textBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.ReadOnly = true;
+            this.textBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox.Size = new System.Drawing.Size(384, 628);
+            this.textBox.TabIndex = 6;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(426, 711);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.buttonStart);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "FormMain";
+            this.Text = "C# Example";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.TextBox textBox;
+    }
+}
+
---
+++ CSharpExample/FormMain.cs
@@ -0,0 +1,170 @@
+﻿// Copyright ©2016 Copper Mountain Technologies
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR
+// ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
+// THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace CSharpExample
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            // --------------------------------------------------------------------------------------------------------
+            /*
+            R54Lib.RVNA app = null;
+
+            // get type
+            Type type = Type.GetTypeFromProgID("RVNA.Application", "localhost");
+            if (type != null)
+            {
+                // create com interface object
+                app = (R54Lib.RVNA)Activator.CreateInstance(type);
+            }
+            */
+            // --------------------------------------------------------------------------------------------------------
+            /*
+            TR1300Lib.TRVNA app = null;
+
+            // get type
+            Type type = Type.GetTypeFromProgID("TRVNA.Application", "localhost");
+            if (type != null)
+            {
+                // create com interface object
+                app = (TR1300Lib.TRVNA)Activator.CreateInstance(type);
+            }
+            */
+            // --------------------------------------------------------------------------------------------------------
+
+            S2VNALib.S2VNA app = null;
+
+            // get type
+            Type type = Type.GetTypeFromProgID("S2VNA.Application", "localhost");
+            if (type != null)
+            {
+                // create com interface object
+                app = (S2VNALib.S2VNA)Activator.CreateInstance(type);
+            }
+
+            // --------------------------------------------------------------------------------------------------------
+            /*
+            S4VNALib.S4VNA app = null;
+
+            // get type
+            Type type = Type.GetTypeFromProgID("S4VNA.Application", "localhost");
+            if (type != null)
+            {
+                // create com interface object
+                app = (S4VNALib.S4VNA)Activator.CreateInstance(type);
+            }
+            */
+            // --------------------------------------------------------------------------------------------------------
+
+            textBox.Clear();
+            textBox.AppendText("Waiting for VNA\n");
+            textBox.AppendText("\n");
+
+            if (app == null)
+            {
+                return;
+            }
+
+            /*
+            int n = 0;
+            while ((app.READy != true) && (n < 100))
+            {
+                Thread.Sleep(100);
+                n++;
+                textBox.AppendText(".");
+            }
+            if (app.READy == false)
+            {
+                return;
+            }
+            */
+
+            int n = 0;
+            while ((app.Ready != true) && (n < 100))
+            {
+                Thread.Sleep(100);
+                n++;
+                textBox.AppendText(".");
+            }
+            if (app.Ready == false)
+            {
+                return;
+            }
+
+            int channelNumber = 1;
+            int traceNumber = 1;
+
+            textBox.AppendText("VNA Ready\n");
+            textBox.AppendText("\n");
+
+            textBox.AppendText("Sending System Preset\n");
+            app.SCPI.SYSTem.PRESet();
+            textBox.AppendText("\n");
+
+            // ========================================================================================================
+
+            // ========================================================================================================
+
+            textBox.AppendText("Setting Trace 1 of Channel 1 to S21\n");
+            app.SCPI.CALCulate[channelNumber].PARameter[traceNumber].DEFine = "S21";
+            textBox.AppendText("\n");
+
+            textBox.AppendText("Retrieving Frequency Data\n");
+            double[] F = app.SCPI.SENSe.FREQuency.DATA;
+            textBox.AppendText("\n");
+
+            textBox.AppendText("Retrieving S21 Parameter Data from Trace 1 of Channel 1\n");
+            double[] S21 = app.SCPI.CALCulate[channelNumber].TRACe[traceNumber].DATA.SDATa;
+            textBox.AppendText("\n");
+
+            // loop thru all frequency points
+            for (int i = 0; i < F.Length; i++)
+            {
+                // print frequency
+                textBox.AppendText("Freq: " + string.Format("{0:0.###}", F[i] / 1000000) + " MHz\n");
+
+                // print real part
+                textBox.AppendText("Real: " + S21[i * 2] + "\n");
+
+                // print imaginary part
+                textBox.AppendText("Imag: " + S21[i * 2 + 1] + "\n");
+
+                textBox.AppendText("\n");
+            }
+
+            app.SCPI.SYSTem.TERMinate();
+
+            /*
+    object err = app.SCPI.CALCulate[1].SELected.FORMat = "DSWR";
+
+    textBox.AppendText("Retrieving Frequency Data\n");
+    double[] F = app.SCPI.SENSe.FREQuency.DATA;
+    textBox.AppendText("\n");
+
+    textBox.AppendText("Retrieving DRLOss Data from Trace 1 of Channel 1\n");
+    double[] DRLOss = app.SCPI.CALCulate[channelNumber].TRACe[traceNumber].DATA.SDATa;
+    textBox.AppendText("\n");
+            */
+        }
+    }
+}
